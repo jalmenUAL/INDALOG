4f42299d9a709dd8d94290519a433cafe4b3d99f
jalmenUAL <50332351+jalmenUAL@users.noreply.github.com>
2019-05-06 19:12:19 +0200

    Update functional.pl

--- EXAMPLES-QUERIES/functional.pl
+++ EXAMPLES-QUERIES/functional.pl
@@ -1,10 +1,4 @@
-{\rtf1\mac\ansicpg10000\cocoartf824\cocoasubrtf420
-{\fonttbl\f0\fswiss\fcharset77 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11899\paperh16839\margl1440\margr1440\vieww9000\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\ql\qnatural\pardirnatural
-
-\f0\fs24 \cf0 ?- evq employer(X)><Y.\
+?- evq employer(X)><Y.\
 ?- evq employer_age(X)><Y.\
 ?- evq address(X)><Y.\
 ?- evq age(X)><Y.\
